6841efd59bc62dab0d352f7665f64abf344f75c7
bbeaucamp <benjamin.beaucamp@univ-eiffel.fr>
2024-08-04 20:25:06 +0200

    Initial Commit

---
+++ PanoViewer/PanoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanoViewerApp.swift
+//  PanoViewer
+//
+//  Created by Benjamin Beaucamp on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PanoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
