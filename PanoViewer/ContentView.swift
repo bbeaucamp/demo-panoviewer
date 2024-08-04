0563213136b45ff6234835659c5d987ba21b867f
bbeaucamp <benjamin.beaucamp@univ-eiffel.fr>
2024-08-04 23:13:00 +0200

    Clean code

--- PanoViewer/ContentView.swift
+++ PanoViewer/ContentView.swift
@@ -15,37 +15,16 @@ struct ContentView: View {
             }
         }
         .frame(minWidth: 300, minHeight: 300)
-        .onDrop(of: [UTType.image, UTType.fileURL], isTargeted: nil) { providers, _ in
-            print("Drag detected")
-            print("Number of providers: \(providers.count)")
-            
-            for (index, provider) in providers.enumerated() {
-                print("Processing provider \(index + 1)")
-                print("Supported identifiers: \(provider.registeredTypeIdentifiers)")
-                
-                for identifier in provider.registeredTypeIdentifiers {
-                    provider.loadItem(forTypeIdentifier: identifier, options: nil) { item, error in
-                        if let error = error {
-                            print("Error loading item for identifier \(identifier): \(error.localizedDescription)")
-                        } else if let data = item as? Data {
-                            print("Data loaded for identifier \(identifier), size: \(data.count) bytes")
-                            DispatchQueue.main.async {
-                                self.viewModel.loadImage(data: data)
-                            }
-                            return
-                        } else if let url = item as? URL {
-                            print("URL loaded for identifier \(identifier): \(url.path)")
-                            DispatchQueue.main.async {
-                                self.viewModel.loadImage(from: url)
-                            }
-                            return
-                        } else {
-                            print("Unknown item type for identifier \(identifier)")
-                        }
+        .onDrop(of: [.image], isTargeted: nil) { providers, _ in
+            guard let provider = providers.first else { return false }
+            _ = provider.loadDataRepresentation(for: .image) { data, error in
+                if error == nil, let data {
+                    DispatchQueue.main.async {
+                        self.viewModel.loadImage(data: data)
                     }
                 }
             }
-            
+                
             return true
         }
     }
